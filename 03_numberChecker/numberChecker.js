39833967d234526e95cfe0f82e9ae6ac81e211ac
jonas99007 <jonasfesehazion@yahoo.de>
2025-05-12 16:31:42 +0200

    test run 03_numberChecker

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,4 +1,24 @@
 function numberChecker(number) {
+ 
+
+  if (number === 1000) {
+    return true;
+  } else {
+    return false;
+  }
+
+  if (number === 10) {
+    return true;
+  } else {
+    return false;
+  }
+
+  if (number === 9) {
+    return true;
+  } else {
+    return false;
+  }
+ 
   if (number === 6) {
     return true;
   } else {
