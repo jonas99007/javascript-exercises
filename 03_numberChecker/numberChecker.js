d46c5b7dbb5a0fe9bd03d42af35d6fa9111e4767
jonas99007 <jonasfesehazion@yahoo.de>
2025-05-12 17:02:10 +0200

    03_numberChecker done

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,30 +1,13 @@
 function numberChecker(number) {
- 
 
-  if (number === 1000) {
-    return true;
-  } else {
-    return false;
-  }
-
-  if (number === 10) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 
-  if (number === 9) {
-    return true;
-  } else {
-    return false;
-  }
- 
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
-  }
+// This is the only if-else statment that we need because we are working with greater than 
+
 }
-
 // Do not edit below this line
 module.exports = numberChecker;
